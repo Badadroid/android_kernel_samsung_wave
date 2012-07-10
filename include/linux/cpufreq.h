311ef19ab7a36182f7cc53654f042a6f34aae1f3
Kasper Hettinga <kasper.hettinga@gmail.com>
2012-07-10 16:25:53 +0200

    Added Wheatley governor
    For more info, see http://rootzwiki.com/topic/14685-kernel07-jul-glados-v134-testing-v20-for-jb/page__st__90__p__405813#entry405813
    
    Change-Id: Id31a308a002a7a909805a1839899a01359f60c6d

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -367,6 +367,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lulzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_wheatley)
 #endif
 
 
