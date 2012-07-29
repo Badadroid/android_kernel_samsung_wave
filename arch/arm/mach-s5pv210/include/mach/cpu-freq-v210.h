8440b191973fe3bf6f5684f5ced02dde077b95de
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-07-29 06:24:14 +0400

    Revert "aries: add OC support"
    This reverts commit 463e99c53ae3b7c18ad3bee3825d4d75c603adae
    
    See Andrew Dodd's comments on http://review.cyanogenmod.com/#/c/19820/.
    Also been getting rare homescreen corruption after moving PVR to DVFS lock.
    
    Change-Id: I628ca36ea63d87b6e74ef346f4acb04d5bfadeae

--- arch/arm/mach-s5pv210/include/mach/cpu-freq-v210.h
+++ arch/arm/mach-s5pv210/include/mach/cpu-freq-v210.h
@@ -15,9 +15,7 @@
 #include <linux/cpufreq.h>
 
 enum perf_level {
-	OC0,
-	L0, L1, L2, L3, L4,
-	MAX_PERF_LEVEL = L4,
+	L0, L1, L2, L3, L4, MAX_PERF_LEVEL = L4,
 };
 
 /* For cpu-freq driver */
@@ -43,7 +41,6 @@ enum {
 	DVFS_LOCK_TOKEN_7,	// 	(TOUCH)
 	DVFS_LOCK_TOKEN_8,	// USB
 	DVFS_LOCK_TOKEN_9,	// BT
-	DVFS_LOCK_TOKEN_10,	// PVR
 	DVFS_LOCK_TOKEN_NUM
 };
 
