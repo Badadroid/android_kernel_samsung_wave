17560f7b731a77ef3eab61f546d0951a45745500
Kasper Hettinga <kasper.hettinga@gmail.com>
2012-10-27 18:38:27 +0200

    Add ability to under- and overvolt.
    Patch set 1: original commit
    Patch set 2: removed spaces
    Patch set 3: increased stock voltage at 1200MHz to 1325mV
    Patch set 4: changed UV interface to allow OV with Voltage Control
    Patch set 5: spacing
    Patch set 6: rebased
    Patch set 7: allow higher OV voltage
    
    Change-Id: Ie5f5d8ca7ed64e63b9be0aa088b99c1b8466b605
    
    Signed-off-by: Nikolay <volk204@mail.ru>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -470,6 +470,13 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 }
 
 /**
+ * undervolting/overclocking
+ * sysfs interface
+ */
+extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf);
+extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy, const char *buf, size_t count);
+
+/**
  * show_scaling_driver - show the cpufreq driver currently loaded
  */
 static ssize_t show_scaling_driver(struct cpufreq_policy *policy, char *buf)
@@ -592,6 +599,7 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
+cpufreq_freq_attr_rw(UV_mV_table);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -605,6 +613,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+	&UV_mV_table.attr,
 	NULL
 };
 
