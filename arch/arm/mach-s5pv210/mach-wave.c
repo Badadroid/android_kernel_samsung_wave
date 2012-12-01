a78f32317ca555d5fa294bace6179213b50142c2
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-12-01 23:56:31 +0700

    aries: Wi-Fi adjustments
    Application of the following 2 commits for aries. Should help
    with Wi-Fi not working for some users.
    
    Symptoms include the following lines in dmesg
    	sdio_reset_comm():
    	mmc1: Error resetting SDIO communications (-110)
    
    commit 00fdd611fdc3d837d55185ece69bee18ca51bd40
    Author: Dmitry Shmidt <dimitrysh@google.com>
    Date:   Wed Jan 26 18:28:44 2011 -0800
    
        ARM: herring: Increase delay to 200 ms in wlan power on
    
        Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    commit 7b8e3bc20dfedd5d55af325c91b79bb4a778193b
    Author: Dmitry Shmidt <dimitrysh@google.com>
    Date:   Tue Nov 8 11:35:15 2011 -0800
    
        ARM: s5pv210: herring: Add delay to wait for wlan carddetect
    
        Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    Change-Id: I470e033aad9706fd705c06f92aebea8aa1210b7f

--- arch/arm/mach-s5pv210/mach-wave.c
+++ arch/arm/mach-s5pv210/mach-wave.c
@@ -4410,7 +4410,7 @@ static int wlan_power_en(int onoff)
 		s3c_gpio_slp_setpull_updown(GPIO_WLAN_BT_EN,
 					S3C_GPIO_PULL_NONE);
 
-		msleep(80);
+		msleep(200);
 	} else {
 		gpio_set_value(GPIO_WLAN_nRST, GPIO_LEVEL_LOW);
 		s3c_gpio_slp_cfgpin(GPIO_WLAN_nRST, S3C_GPIO_SLP_OUT0);
@@ -4460,6 +4460,7 @@ static int wlan_carddetect_en(int onoff)
 	udelay(5);
 
 	sdhci_s3c_force_presence_change(&s3c_device_hsmmc1);
+	msleep(500); /* wait for carddetect */
 	return 0;
 }
 
