5a7c8d46f968574c474df8fe4f27be2cd5314697
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-06-26 10:52:00 +0100

    Lagfree CPU Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -365,6 +365,9 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lagfree)
 #endif
 
 
