f1165f2c0a3f58d60ec0bbb3253a932f93cc8207
Humberto Borba <humberos@gmail.com>
2013-07-01 10:52:47 -0300

    drivers: media: radio: si4709: update power management
    <4>[    4.722171] i2c-core: driver [Si4709] using legacy suspend method
    <4>[    4.722351] i2c-core: driver [Si4709] using legacy resume method
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I5e4a92bed7bb53f351985b9ef6c97ff7967e6de3

--- drivers/media/radio/si4709/radio-si4709_i2c_drv.c
+++ drivers/media/radio/si4709/radio-si4709_i2c_drv.c
@@ -13,8 +13,8 @@ void Si4709_i2c_drv_exit(void);
 /*static functions*/
 static int Si4709_probe (struct i2c_client *);
 static int Si4709_remove(struct i2c_client *);
-static int Si4709_suspend(struct i2c_client *, pm_message_t mesg);
-static int Si4709_resume(struct i2c_client *);
+static int Si4709_suspend(struct device *dev);
+static int Si4709_resume(struct device *dev);
 
 static struct i2c_client *Si4709_i2c_client;
 
@@ -120,57 +120,45 @@ static int __devexit si4709_i2c_remove(struct i2c_client *client)
 
 MODULE_DEVICE_TABLE(i2c, si4709_id);
 
-static struct i2c_driver Si4709_i2c_driver =
-{
-    	.driver = {
-		.owner = THIS_MODULE,
-        	.name = "Si4709",
-    	},
-	.id_table = si4709_id,
-	.probe = si4709_i2c_probe,
-	.remove = __devexit_p(si4709_i2c_remove),
-    	.suspend = Si4709_suspend,
-    	.resume = Si4709_resume,
-	.address_list = Si4709_normal_i2c,
-};
-
-static int Si4709_suspend(struct i2c_client *client, pm_message_t mesg)
+static int Si4709_suspend(struct device *dev)
 {
     int ret = 0;
 	   
     debug("Si4709 i2c driver Si4709_suspend called"); 
 
-    if( strcmp(client->name, "Si4709") != 0 )
-    {
-        ret = -1;
-        debug("Si4709_suspend: device not supported");
-    }
-    else if( (ret = Si4709_dev_suspend()) < 0 )
-    {
+	if ((ret = Si4709_dev_suspend()) < 0 )
         debug("Si4709_dev_disable failed");
-    }
 
     return 0;
 }
 
-static int Si4709_resume(struct i2c_client *client)
+static int Si4709_resume(struct device *dev)
 {
     int ret = 0;
 	   
-//    debug("Si4709 i2c driver Si4709_resume called"); 
-
-    if( strcmp(client->name, "Si4709") != 0 )
-    {
-        ret = -1;
-        debug("Si4709_resume: device not supported");
-    }
-    else if( (ret = Si4709_dev_resume()) < 0 )
-    {
+	if ((ret = Si4709_dev_resume()) < 0 )
         debug("Si4709_dev_enable failed");
-    }
  
     return 0;
 }
+
+static const struct dev_pm_ops Si4709_pm_ops = {
+	.suspend = Si4709_suspend,
+	.resume = Si4709_resume,
+};
+
+static struct i2c_driver Si4709_i2c_driver =
+{
+	.driver = {
+		.owner = THIS_MODULE,
+    	.name = "Si4709",
+		.pm = &Si4709_pm_ops,
+	},
+	.id_table = si4709_id,
+	.probe = si4709_i2c_probe,
+	.remove = __devexit_p(si4709_i2c_remove),
+	.address_list = Si4709_normal_i2c,
+};
 
 int Si4709_i2c_drv_init(void)
 {	
