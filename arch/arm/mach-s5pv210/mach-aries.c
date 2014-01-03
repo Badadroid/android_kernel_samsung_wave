708b3ef6e6f9df78f7188d498f0b828f93412813
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-01-03 19:10:01 +0700

    aries: optimize JPEG memory configuration
    Only 1MB is required for encoding of VGA JPEG images (used by FFC).
    Note that thumbnail encoding function is not used.
    
    Requires userspace change (http://review.cyanogenmod.org/56795).
    
    Change-Id: Icea2fc74dfc692d98ae041b785238d6b847c4ddd

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -320,8 +320,7 @@ static struct s3cfb_lcd s6e63m0 = {
 					     (CONFIG_FB_S3C_NR_BUFFERS + \
 						 (CONFIG_FB_S3C_NUM_OVLY_WIN * \
 						  CONFIG_FB_S3C_NUM_BUF_OVLY_WIN)))
-// Was 8M, but we're only using it to encode VGA jpegs
-#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG (4096 * SZ_1K)
+#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG (916 * SZ_1K)
 #define  S5PV210_ANDROID_PMEM_MEMSIZE_PMEM (5550 * SZ_1K)
 #define  S5PV210_ANDROID_PMEM_MEMSIZE_PMEM_GPU1 (3000 * SZ_1K)
 #define  S5PV210_ANDROID_PMEM_MEMSIZE_PMEM_ADSP (1500 * SZ_1K)
@@ -2351,10 +2350,10 @@ static struct s3c_platform_fimc fimc_plat_lsi = {
 
 #ifdef CONFIG_VIDEO_JPEG_V2
 static struct s3c_platform_jpeg jpeg_plat __initdata = {
-	.max_main_width	= 800,
+	.max_main_width	= 640,
 	.max_main_height	= 480,
-	.max_thumb_width	= 320,
-	.max_thumb_height	= 240,
+	.max_thumb_width	= 0,
+	.max_thumb_height	= 0,
 };
 #endif
 
